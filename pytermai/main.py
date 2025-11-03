61aefd3450b009ec81bef2ba9924a804cf0d8f03
kazisean <keh8423@nyu.edu>
2025-11-03 14:57:45 -0500

    added bloomberg as ascii

--- pytermai/main.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-client = OpenAI(api_key="...")
-stream = client.chat.completions.create(
-    model="gpt-4",
-    messages=[{"role": "user", "content": "hello world"}],
-    stream=True,
-)
-for part in stream:
-    print(part.choices[0].delta.content or "")
