907259de54bd696162f5bb0e9c851ba483bd9f92
TawhidZGit <Tawhid.Zamann@gmail.com>
2025-11-07 22:44:40 -0500

    Added study_tip func

--- src/bloomsays/wisdom.py
+++ src/bloomsays/wisdom.py
@@ -3,6 +3,18 @@ import textwrap
 from pathlib import Path
 from .bubble import make_bubble
 
+allJokes = [
+        "I was about to crack a joke on Ubuntu’s text editor, but you might not gedit.",
+        "I’d tell them a UDP joke but there’s no guarantee that they would get it.",
+        "When I wrote this, only God and I understood what I was doing. Now, God only knows.",
+        "#define TRUE FALSE //Happy debugging suckers",
+        "Which body part does a programmer know best? -> ARM",
+        "What do you call a busy waiter? -> A server.",
+        "What do you call an idle server? -> A waiter",
+        "!false -> It's funny 'cause it's true."
+        ]
+# source : https://zriyansh.medium.com/top-programming-jokes-that-will-make-your-day-or-night-6d986b338f2d
+# https://github.com/wesbos/dad-jokes
 
 
 ascii_art = r"""
@@ -111,19 +123,6 @@ def coding_wisdom(language="Python"):
     return message
 
 def jokes (n=1):
-    # source : https://zriyansh.medium.com/top-programming-jokes-that-will-make-your-day-or-night-6d986b338f2d
-    # https://github.com/wesbos/dad-jokes
-    allJokes = [
-        "I was about to crack a joke on Ubuntu’s text editor, but you might not gedit.",
-        "I’d tell them a UDP joke but there’s no guarantee that they would get it.",
-        "When I wrote this, only God and I understood what I was doing. Now, God only knows.",
-        "#define TRUE FALSE //Happy debugging suckers",
-        "Which body part does a programmer know best? -> ARM",
-        "What do you call a busy waiter? -> A server.",
-        "What do you call an idle server? -> A waiter",
-        "!false -> It's funny 'cause it's true."
-        ]
-
     randomSelect = random.choices(allJokes, k=n)
     
     bubble_text = "\n".join(randomSelect)
